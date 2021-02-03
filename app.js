efa7600eccbbcd6b424b58625368bd38f5d2bc0f
Jennifer <jennifacarey@gmail.com>
2021-02-03 16:09:37 +0000

    eventListener to remove initial text

---
+++ app.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const initialContent = document.querySelector(".initial-content");
+
+document.addEventListener("keydown", () => {
+   initialContent.style.display = "none";
+})
+
