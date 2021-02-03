da33e51754cbaf624e1b5fda83712b73129cd5bf
Jennifer <jennifacarey@gmail.com>
2021-02-03 17:47:03 +0000

    updated eventListener to include event object

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const body = document.querySelector("body");
 const initialContent = document.querySelector(".initial-content");
 
-document.addEventListener("keydown", () => {
+document.addEventListener("keydown", (event) => {
    initialContent.style.display = "none";
+   console.log(event);
+   // console.log(event.target);
 })
 
