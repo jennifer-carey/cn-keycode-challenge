cdffe6dd5b6569cf4bf0c68839ae7b565a851415
Jennifer <jennifacarey@gmail.com>
2021-02-04 09:34:32 +0000

    add console logs to test code

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const initialContent = document.querySelector(".initial-content");
 document.addEventListener("keydown", (event) => {
    initialContent.style.display = "none";
    console.log(event);
+   console.log(event.which);
+   console.log(event.key);
+   console.log(event.code);
+   console.log(event.which);
    // console.log(event.target);
 })
 
