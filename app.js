a4f85168ff56bfcc56efa432ec26ddca81d8370e
Jennifer <jennifacarey@gmail.com>
2021-02-04 09:44:08 +0000

    add variables to keycodes

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const initialContent = document.querySelector(".initial-content");
 
 document.addEventListener("keydown", (event) => {
    initialContent.style.display = "none";
-   console.log(event);
-   console.log(event.which);
-   console.log(event.key);
-   console.log(event.code);
-   console.log(event.which);
-   // console.log(event.target);
+   //console.log(event);
+   let keyNumber = event.which;
+   console.log(keyNumber);
+   let whichKey = event.key;
+   console.log(whichKey);
+   let keyCode = event.code;
+   console.log(keyCode);
+   console.log(keyNumber);
+
 })
 
