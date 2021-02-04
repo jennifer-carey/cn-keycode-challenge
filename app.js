94989a5f4f5f50e126dd250958be06c7ac0673cf
Jennifer <jennifacarey@gmail.com>
2021-02-04 09:59:59 +0000

    textContent to add key info to DOM

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const body = document.querySelector("body");
 const initialContent = document.querySelector(".initial-content");
+const mainKeycode = document.querySelector(".keycode-info");
+const keyCard = document.querySelector(".key-card");
+const codeCard = document.querySelector(".code-card");
+const whichCard = document.querySelector(".which-card");
 
 document.addEventListener("keydown", (event) => {
    initialContent.style.display = "none";
    //console.log(event);
    let keyNumber = event.which;
-   console.log(keyNumber);
+   //console.log(keyNumber);
    let whichKey = event.key;
-   console.log(whichKey);
-   let keyCode = event.code;
-   console.log(keyCode);
-   console.log(keyNumber);
-
+   //console.log(whichKey);
+   let keycode = event.code;
+   //console.log(keyCode);
+   //console.log(keyNumber);
+   mainKeycode.textContent = `${keyNumber}`;
+   keyCard.textContent = `${whichKey}`;
+   codeCard.textContent = `${keycode}`;
+   whichCard.textContent = `${keyNumber}`;
 })
 
