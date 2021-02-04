e4663117cc8e6b5a87c841e2075ac20a28e888fb
Jennifer <jennifacarey@gmail.com>
2021-02-04 12:09:59 +0000

    refactor js code

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const body = document.querySelector("body");
 const initialContent = document.querySelector(".initial-content");
 const mainKeycode = document.querySelector(".keycode-info");
 const keyCard = document.querySelector(".key-card");
@@ -14,7 +13,6 @@ document.addEventListener("keydown", (event) => {
    //console.log(whichKey);
    let keycode = event.code;
    //console.log(keyCode);
-   //console.log(keyNumber);
    mainKeycode.textContent = `${keyNumber}`;
    keyCard.innerHTML = `event.key<br> ${whichKey}`;
    codeCard.innerHTML = `event.code<br> ${keycode}`;
